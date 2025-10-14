afc67bd54672b2014d1bc837334c1c836604c4e8
lab-vic <numata@v-victory.com>
2025-10-14 21:03:32 +0900

    Add files via upload

---
+++ labo/laboinfo.js
@@ -0,0 +1,21 @@
+window.victorylaboinfo = {
+    company:"会社名を㈱など省略せずに入力してください。",
+    division:"",
+    address1:"",
+    department:"",
+    name:"",
+    contact:"",
+    mailaddress:"",
+    reportname:"",
+    
+    reportcase:"",
+    onsite:"",
+    onsite_address:"",
+    manage_name:"",
+    plan:"",
+    resend:"",
+    price:{
+        normal:20000,
+        special:35000,
+    },
+}
